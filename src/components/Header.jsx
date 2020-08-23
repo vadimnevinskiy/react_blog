246a73c952d88f146ad1fd1c065694e9015d46b5
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2020-08-24 00:36:33 +0500

    'init'

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src={"https://p7.hiclipart.com/preview/791/139/716/react-native-web-application-javascript-google-scholar-logo.jpg"}/>
+
+            {/*<img src={"../img/logo.jpg"}/>*/}
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://funik.ru/wp-content/uploads/2019/05/b9b57d4863b903af6a95.jpg" alt=""/>
+            </div>
+            <div className='user'>
+                <img src="https://i007.fotocdn.net/s123/2950fc2e1fadccab/user_xl/2816616767.jpg" alt=""/>
+                <div>
+                    img descr
+                </div>
+            </div>
+            <div className='post'>
+                <div className="postText">
+                    my post
+                </div>
+                <div className='form'>
+                    <textarea type="text" className='textField'></textarea>
+                    <div className='buttonBlock'>
+                        <input type="button" className='button' value='Send' />
+                    </div>
+                </div>
+                <div className='post-list'>
+                    <div className="postText">post 111111</div>
+                    <div className="postText">post 222222</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
