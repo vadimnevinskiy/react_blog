c71f329f05e42a558749fb1325d0f61d6c784a9a
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2020-09-05 19:13:54 +0500

    State.js renamed at store.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/state";
+import store from "./redux/store";
 
 import App from './App';
 
